527a8418fa944ae50f2fc02f091e1a254e598e03
k-bhuva <kunjbhuva.me@gmail.com>
2025-11-27 10:51:56 +0530

    Fixed controller class separation

--- src/main/java/com/app/demo/DemoApplication.java
+++ src/main/java/com/app/demo/DemoApplication.java
@@ -1,8 +1,12 @@
-@RestController
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello DevOps!";
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
 
